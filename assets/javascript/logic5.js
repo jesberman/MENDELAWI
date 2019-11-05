912861c45c11efa7edd5b2ad6d3638d2950e980e
jesberman <jesberman@gmail.com>
2019-11-04 21:47:35 -0500

    added-collections-page-and-other-significant-additions

---
+++ assets/javascript/logic5.js
@@ -0,0 +1,55 @@
+function menuFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+
+$(document).ready(function () {
+    // Navbar Instructions
+    $(".nav-collections-share").mouseover(function () {
+        $("#nav-collections-div").removeClass("hide");
+        $("#nav-collections-div").addClass("show");
+
+    });
+    $(".nav-collections-share").mouseout(function () {
+        $("#nav-collections-div").removeClass("show");
+        $("#nav-collections-div").addClass("hide");
+    });
+
+
+});
+
+
+var detailOneClicked = false;
+var detailTwoClicked = false;
+var detailThreeClicked = false;
+
+
+$("#detail1").click(function () {
+    // detailOneClicked = true;
+    // detailTwoClicked = false;
+    // detailThreeClicked = false;
+    // if (detailOneClicked === true) {
+        $('#detail4').css('background-image',
+            'url("assets/images/looks/look5a.jpg")');
+
+
+    // }
+
+});
+
+$("#detail2").click(function () {
+        $('#detail4').css('background-image',
+            'url("assets/images/looks/look5b.jpg")');
+
+});
+
+$("#detail3").click(function () {
+    $('#detail4').css('background-image',
+        'url("assets/images/looks/look5c.jpg")');
+
+});
